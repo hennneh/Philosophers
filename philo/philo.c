a80689586763f72f189a409e4a3d1bd8d34e5bb2
hennneh <hendrik.lehmann96@gmail.com>
2021-12-08 16:39:05 +0100

    adding linked list

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 19:35:53 by hlehmann          #+#    #+#             */
-/*   Updated: 2021/12/08 13:42:42 by hlehmann         ###   ########.fr       */
+/*   Updated: 2021/12/08 16:05:41 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_init(int argc, char **argv, t_t *t)
 {
 	if (argc < 6 || argc > 7)
 		return (1);
-	t->philos = ft_atoi(argv[1]);
+	t->p = ft_atoi(argv[1]);
 	return (0);
 }
 
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 13:32:57 by hlehmann          #+#    #+#             */
-/*   Updated: 2021/12/08 13:33:01 by hlehmann         ###   ########.fr       */
+/*   Updated: 2021/12/08 16:38:32 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,24 @@
 # include <stdio.h>
 # include <pthread.h>
 
+typedef struct s_p
+{
+	int	lm; //time since last meal
+	int	te; //times eaten
+	int	tn; //threadnumber
+	pthread_mutex_t	f; //fork
+	t_p *nl; //next element in the linked list
+}	t_p;
+
 typedef struct s_t
 {
-	int	philos;
-	int	cycles;
-	int	tts;
-	int	tte;
-	int	ttd;
-	int	eaten;
-	float	time;
+	int	p; //number of philosophers
+	int	ttd; //time to die
+	int	tte; //time to eat
+	int	tts; //time to sleep
+	int	cycles; //how many times to eat
+	float	st; //start time of the program
+	t_l	philos; //linked list with the philosophers
 }	t_t;
 
 int	ft_atoi(const char *nptr);
