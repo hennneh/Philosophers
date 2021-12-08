3ff51798672f26f3f138f32d9fdddef33f841298
hennneh <hendrik.lehmann96@gmail.com>
2021-12-08 13:42:50 +0100

    adding headers

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,22 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 19:35:53 by hlehmann          #+#    #+#             */
-/*   Updated: 2021/12/07 19:56:47 by hlehmann         ###   ########.fr       */
+/*   Updated: 2021/12/08 13:42:42 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	ft_init(int argc, char **argv, t_t *t)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
+	t_t	t;
+
+	if (ft_init(argc, argv, &t))
+		ft_error();
 	return (0);
 }
--- test.c
+++ test.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+
+char *s = "hello kostas";
 
 void	*routine()
 {
-	printf("hello world\n");
+	int i = 0;
+	while (i < 9)
+	{
+	write(1, &s[i], 1);
+	i++;
+	}
 	return (0);
 }
 
 int	main(void)
 {
 	pthread_t t1;
+	pthread_t t2;
 
 	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_create(&t2, NULL, &routine, NULL);
 	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 	return (0);
 }
