9cc6f650cca1c9507c3fec5948a4dbb3b355e7d8
hennneh <hendrik.lehmann96@gmail.com>
2021-12-09 15:58:05 +0100

    renaming variables

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 13:32:57 by hlehmann          #+#    #+#             */
-/*   Updated: 2021/12/09 14:32:15 by hlehmann         ###   ########.fr       */
+/*   Updated: 2021/12/09 15:57:44 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,25 @@
 # include <stdlib.h>
 # include <unistd.h>		//remove after testing aöksdjföaskjdföajksdöfjkasöldkjfalöskdjflöaskdjfölaskdjf
 
-typedef struct philo
-{
-	int	meals_eaten;
-	int	time_since_last_meal;
-}	t_philo;
-
 typedef struct s_t
 {
-	int				p; //number of philosophers
-	int				ttd; //time to die
-	int				tte; //time to eat
-	int				tts; //time to sleep
-	int				m; //how many times to eat aka meals
-	struct timeval	st; //starting time of the program
-	struct timeval	ct; //current time
-	pthread_t		*gl; //guest list or array for threads aka philosophers
-	pthread_mutex_t	*f; //array of mutex which will act as forks
-	t_philo	*array;
+	int				nphil;
+	int				t2d;
+	int				t2e;
+	int				t2s;
+	int				nmeal; //how many times to eat aka meals
+	struct timeval	stime; //starting time of the program
+	pthread_t		*parr; //array for the threads aka philosophers
+	pthread_mutex_t	*frk;
 }	t_t;
+
+typedef struct s_philo
+{
+	int	meals_eaten;
+	int	last_meal_time;
+	struct timeval	currenttime;
+	t_t	*t;
+}	t_philo;
 
 int		ft_atoi(const char *nptr);
 long	ft_atol(char *s);
