ea69db7931b214f73e72fb46511c75399dc2a8fa
hennneh <hendrik.lehmann96@gmail.com>
2022-02-01 15:15:35 +0100

    starting threads function

--- v1/philo.c
+++ v1/philo.c
@@ -6,11 +6,23 @@
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 08:28:08 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/02/01 10:19:01 by hlehmann         ###   ########.fr       */
+/*   Updated: 2022/02/01 14:16:38 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	start_threads(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,7 +33,7 @@ int	main(int argc, char **argv)
 	if (init_struct(argc, argv, &table))
 		return (1);
 	printf("%lld\n", table.start_time);
-//	if (start_threads(&table))
-//		return (1);
+	if (start_threads(&table))
+		return (1);
 	return (0);
 }
