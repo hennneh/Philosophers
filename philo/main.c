ab9e42e271c7c3d6ee0b4c3bf4ae15c0cfd6865d
hennneh <hendrik.lehmann96@gmail.com>
2022-03-10 10:59:43 +0100

    final version

--- philo/main.c
+++ philo/main.c
@@ -5,97 +5,76 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/01 08:28:08 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/02/18 16:12:22 by hlehmann         ###   ########.fr       */
+/*   Created: 2022/02/09 09:03:03 by hlehmann          #+#    #+#             */
+/*   Updated: 2022/03/09 10:49:48 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "philosophers.h"
 
-void	*routine(void *philo_ptr)
+int	ft_free(t_table *table)
 {
-	t_philo	*philo;
-
-	philo = philo_ptr;
-	if (philo->number % 2 == 0)
-		naptime(philo, philo->table->tte);
-	while (!philo->table->full)
-	{
-		pthread_mutex_lock(&philo->fork_one);
-		fork_action(philo);
-		pthread_mutex_lock(&philo->fork_two);
-		fork_action(philo);
-		food_action(philo);
-		naptime(philo, philo->table->tte);
-		pthread_mutex_unlock(&philo->fork_one);
-		pthread_mutex_unlock(&philo->fork_two);
-		bed_action(philo);
-		naptime(philo, philo->table->tts);
-		thought_action(philo);
-	}
-	printf("test %d\n", philo->number);
-	return (NULL);
+	if (table->forks)
+		free (table->forks);
+	if (table->philo)
+		free (table->philo);
+	return (1);
 }
 
-void	init_philo(t_table *table)
-{
-	int		i;
-	t_philo	*philo;
-
-	i = 0; 
-	while (i < table->nb_philo)
-	{
-		philo = &table->philo[i];
-		philo->table = table;
-		philo->number = i + 1;
-		philo->last_meal = table->start_time;
-		philo->alive = 1;
-		philo->nb_meals = 0;
-		philo->fork_one = table->forks[i];
-		i++;
-		philo->fork_two = table->forks[i % table->nb_philo];
-	}
-}
-
-void	wait_until_full(t_table *table)
-{
-	/*Üusleep(10000000);
-	printf("waiting in main\n");
-	printf("start time is: %lld\n", table->start_time);*/
-	while (1)
-	{
-		pthread_mutex_lock(&table->status);
-		if (check_status(table))
-		{
-			table->full = 1;
-			pthread_mutex_unlock(&table->status);
-			break ;
-		}
-		pthread_mutex_unlock(&table->status);
-	}
-}
-
-int	start_threads(t_table *table)
+int	ft_exit(t_table *table)
 {
 	int		i;
 	t_philo	*philo;
 
 	i = 0;
-	init_philo(table);
-	table->start_time = get_start_time();
 	while (i < table->nb_philo)
 	{
 		philo = &table->philo[i];
-		if (pthread_create(&philo->thread_id, NULL, routine, philo))
-		{
-			pthread_mutex_lock(&table->status);
-			table->full = 1;
-			pthread_mutex_unlock(&table->status);
-			return (1);
-		}
+		pthread_join(philo->thread_id, NULL);
 		i++;
 	}
-	wait_until_full(table);
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		pthread_mutex_destroy(&table->forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(&table->status);
+	return (ft_free(table));
+}
+
+static int	is_valid(char *argv)
+{
+	int	i;
+
+	i = 0;
+	if (!argv || !argv[i])
+		return (1);
+	while (argv[i])
+	{
+		if (argv[i] < '0' || argv[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	error_check(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc < 5 || argc > 6)
+	{
+		write (2, "Error: Input\n", 13);
+		return (1);
+	}
+	while (argv[i])
+	{
+		if (is_valid(argv[i]) == 1)
+			return (1);
+		i++;
+	}
 	return (0);
 }
 
@@ -103,14 +82,14 @@ int	main(int argc, char **argv)
 {
 	t_table	table;
 
-	if (check_input(argc, argv))
+	if (!error_check(argc, argv))
 	{
-		printf("Error: Input\n");
-		return (1);
+		if (set_table(argc, argv, &table) == 1)
+			return (1);
+		if (lunch(&table) == 1)
+			return (ft_exit(&table));
 	}
-	if (init_struct(argc, argv, &table))
+	else
 		return (1);
-	if (start_threads(&table))
-		return (1);
-	return (0);
+	return (ft_exit(&table) * 0);
 }
--- philo/utils.c
+++ philo/utils.c
@@ -5,66 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlehmann <hlehmann@student.42wolfsburg.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/01 08:27:58 by hlehmann          #+#    #+#             */
-/*   Updated: 2022/02/01 10:32:05 by hlehmann         ###   ########.fr       */
+/*   Created: 2022/02/09 09:02:55 by hlehmann          #+#    #+#             */
+/*   Updated: 2022/03/09 10:50:04 by hlehmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "philosophers.h"
 
-int	ft_atoi(const char *nptr)
+void	ft_usleep(t_table *table, long long time_limit)
 {
-	size_t	i;
-	int		sign;
-	int		num;
+	long long	start;
 
-	i = 0;
-	sign = 1;
-	num = 0;
-	while ((nptr[i] > 8 && nptr[i] < 14) || nptr[i] == 32)
-		i++;
-	if (nptr[i] == 43 || nptr[i] == 45)
+	start = ft_get_time();
+	while (ft_get_time() - start < time_limit / 1000)
 	{
-		if (nptr[i] == 45)
-			sign *= -1;
-		i++;
+		if (table->nb_philo < 100)
+			usleep(100);
+		else
+			usleep(1000);
 	}
-	while (nptr[i] > 47 && nptr[i] < 58)
-	{
-		num = num * 10 + nptr[i] - '0';
-		i++;
-	}
-	return (num * sign);
 }
 
-long	ft_atol(char *s)
+long long	ft_get_time(void)
 {
-	long	nbr;
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
+long	ft_atol(const char *str)
+{
+	long	result;
+	size_t	res;
+	int		negative;
 	int		i;
-	int		sign;
 
-	nbr = 0;
+	negative = 1;
+	res = 0;
 	i = 0;
-	sign = 1;
-	if (s[i] == '-')
-	{
+	while (str[i] && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
 		i++;
-		sign = -1;
-	}
-	while (s[i])
+	if (str[i] == '-' || str[i] == '+')
 	{
-		nbr = nbr * 10 + s[i] - '0';
+		if (str[i] == '-')
+			negative = -negative;
 		i++;
 	}
-	return (nbr * sign);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	result = res * negative;
+	return (result);
 }
 
-size_t	ft_strlen(const char *str)
+void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
+	char	*str;
 
+	str = s;
 	i = 0;
-	while (str[i] != '\0')
+	while (i < n)
+	{
+		str[i] = '\0';
 		i++;
-	return (i);
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = malloc(count * size);
+	if (!result)
+		return (NULL);
+	ft_bzero(result, count * size);
+	return (result);
 }
